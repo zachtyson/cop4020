f18fa29efce74b4086ebee5bce7a0af82c86311e
Zachary <tysonzachary@ufl.edu>
2023-02-20 03:04:49 -0500

    Added extra text

--- src/edu/ufl/cise/plcsp23/implementation/IParserImplementation.java
+++ src/edu/ufl/cise/plcsp23/implementation/IParserImplementation.java
@@ -1,19 +1,56 @@
 package edu.ufl.cise.plcsp23.implementation;
 
+import edu.ufl.cise.plcsp23.exceptions.LexicalException;
 import edu.ufl.cise.plcsp23.exceptions.SyntaxException;
 import edu.ufl.cise.plcsp23.interfaces.IParser;
 import edu.ufl.cise.plcsp23.exceptions.PLCException;
 import edu.ufl.cise.plcsp23.ast.AST;
 
-public class IParserImplementation implements IParser {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Objects;
 
+public class IParserImplementation implements IParser {
+    private ArrayList<AST> ASTList = new ArrayList<AST>();
     @Override
     public AST parse() throws PLCException {
-        return null;
+        if(ASTList.size() == 0) {
+            throw new SyntaxException("No ASTs to parse");
+        }
+        return ASTList.get(0);
     }
 
-    public IParserImplementation(String input) throws SyntaxException {
-        // TODO Auto-generated constructor stub
+    //expr = conditional_expr | or_expr
+    //conditional_expr = if expr ? expr ? expr
+    //or_expr = and_expr (|/|| and_expr)* -- At least one and_expr followed by zero or more |/|| and_expr
+    //and_expr comparison_expr (&/&& comparison_expr)* -- At least one comparison_expr followed by zero or more &/&& comparison_expr
+    //comparison_expr = power_expr (</>/==/<=/>= power_expr)* -- At least one power_expr followed by zero or more ==/!=/</>/<=/>= power_expr
+    //power_expr = additive_expr ** power_expr | additive_expr -- At least one additive_expr followed by ** and either another power_expr or an additive_expr
+    //additive_expr = multiplicative_expr (+/- multiplicative_expr)* -- At least one multiplicative_expr followed by zero or more +/- multiplicative_expr
+    //multiplicative_expr = unary_expr (*//% unary_expr)* -- At least one unary_expr followed by zero or more */% unary_expr
+
+    public IParserImplementation(String input) throws SyntaxException, LexicalException, PLCException {
+        if(input == null || input.isEmpty()) {
+            throw new SyntaxException("Input is null or empty");
+        }
+        //Create a scanner
+        IScannerImplementation scanner = new IScannerImplementation(input);
+        //Pass the input to the scanner
+        while (true) {
+            try {
+
+            } catch (Exception e) {
+                if(e.getMessage().equals("Number out of bounds")) {
+                    throw new SyntaxException("Number out of bounds");
+                    //I don't think this will ever happen given the nature of Assignment 2
+                } else if(e.getMessage().equals("No tokens")) {
+                    //This should only happen when there is an empty input, and it somehow gets past the null check
+                    throw new SyntaxException("Input is null or empty");
+                }
+                break;
+            }
+        }
+
         throw new SyntaxException("Not yet implemented");
     }
 }
