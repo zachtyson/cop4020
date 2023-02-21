43edbaa7b71ae99ae1b7ad1fd81c63d7c5213a34
Zachary <tysonzachary@ufl.edu>
2023-02-21 02:54:33 -0500

    Literally no idea where these came from

---
+++ src - Copy/edu/ufl/cise/plcsp23/INumLitToken.java
@@ -0,0 +1,21 @@
+/*Copyright 2023 by Beverly A Sanders
+ *
+ * This code is provided for solely for use of students in COP4020 Programming Language Concepts at the
+ * University of Florida during the spring semester 2023 as part of the course project.
+ *
+ * No other use is authorized.
+ *
+ * This code may not be posted on a public web site either during or after the course.
+ */
+
+package edu.ufl.cise.plcsp23;
+
+public interface INumLitToken extends IToken {
+
+	int getValue();
+	//A Num_Lit token can either be a 0, or a non-zero digit followed by a sequence of digits (can't start with 0 and can't be empty)
+	//In this project, the number is a java integer, so the bounds are -2^31 to 2^31-1
+	//I'm not really experienced with interfaces but I think in this case it's identical to IToken except it has an associated value
+
+
+}
