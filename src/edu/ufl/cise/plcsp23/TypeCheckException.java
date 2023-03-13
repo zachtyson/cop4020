81a6cadd10d2a13a2b7bcfd74cdfcd62717af425
Zachary <tysonzachary@ufl.edu>
2023-03-13 00:13:53 -0400

    Created TypeCheckException.java

---
+++ src/edu/ufl/cise/plcsp23/TypeCheckException.java
@@ -0,0 +1,9 @@
+package edu.ufl.cise.plcsp23;
+
+public class TypeCheckException extends PLCException{
+
+        public TypeCheckException(String message) {
+            super(message);
+        }
+
+}
