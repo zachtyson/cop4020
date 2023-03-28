22f10a5618a649f8b5b7485dd8c5cd361c0eb744
Zachary <tysonzachary@ufl.edu>
2023-03-28 17:06:57 -0400

    Updated SymbolTable for better scope handling

--- src/edu/ufl/cise/plcsp23/implementation/SymbolTable.java
+++ src/edu/ufl/cise/plcsp23/implementation/SymbolTable.java
@@ -9,11 +9,13 @@ import java.util.HashMap;
 import java.util.List;
 
 public class SymbolTable {
-    private final HashMap<String, NameDef> symbolMap;
+    private final HashMap<String, Symbol> symbolMap;
+
+    private int currentScope = 0;
 
     public SymbolTable(List<NameDef> paramList) throws PLCException{
         //Honestly I don't even think I need to put anything in here
-        symbolMap = new HashMap<String, NameDef>();
+        symbolMap = new HashMap<String, Symbol>();
         for(NameDef n : paramList) {
             Type paramType = n.getType();
             if(paramType == Type.VOID) {
@@ -23,51 +25,45 @@ public class SymbolTable {
                 throw new TypeCheckException("Parameter " + symbolMap + " is already defined");
             }
             else {
-                symbolMap.put(n.getIdent().getName(), n);
+                symbolMap.put(n.getIdent().getName(), new Symbol(n, currentScope));
             }
         }
 
     }
 
     public SymbolTable(SymbolTable parent) throws PLCException{
-        symbolMap = new HashMap<String, NameDef>();
+        currentScope = parent.currentScope + 1;
+        symbolMap = new HashMap<String, Symbol>();
         symbolMap.putAll(parent.symbolMap);
-        if(symbolMap.containsKey("1")) {
-            INumLitImplementation numLit2 = new INumLitImplementation(null,null,symbolMap.get("1").getFirstToken().getSourceLocation().line(),0);
-            NameDef nameDef2 = new NameDef(numLit2,null,null,null);
-            symbolMap.replace("1", nameDef2);
-        } else {
-            INumLitImplementation numLit = new INumLitImplementation(null,null,1,0);
-            NameDef nameDef = new NameDef(numLit,null,null,null);
-            symbolMap.put("1", nameDef);
-        }
     }
 
     public NameDef get(String s) throws PLCException {
-        return symbolMap.get(s);
+        Symbol symbol = symbolMap.get(s);
+        if(symbol == null) {
+            return null;
+        }
+        NameDef nameDef = symbol.getNameDef();
+        return nameDef;
     }
 
     public void put(String s, NameDef n) throws PLCException{
-        if(symbolMap.containsKey(s)) {
-            //Honestly I don't know what to do here, since test 17 makes it so that the same identifier name can be used in different scopes
-            //If I had any say in this I would forbid shadowing, but I don't so I guess I'll just have to deal with it
-            //todo: dear god please fix this
-            if (symbolMap.containsKey("1")) {
-                //If this is an nested scope, then we can just allow it
-                symbolMap.replace(s, n);
-            }
-            else {
-                throw new TypeCheckException("Identifier " + s + " already exists in the symbol table, error at line " + s + ", column " + s);
-            }
+        Symbol symbol = symbolMap.get(s);
+        if(symbol == null) {
+            symbolMap.put(s, new Symbol(n, currentScope));
         }
         else {
-            symbolMap.put(s, n);
+            if(currentScope == symbol.getScope()) {
+                throw new TypeCheckException("Variable " + s + " is already defined");
+            }
+            else {
+                symbolMap.put(s, new Symbol(n, currentScope));
+            }
         }
     }
 
     public void put(List<NameDef> paramList) throws PLCException {
         for(NameDef n : paramList) {
-            symbolMap.put(n.getIdent().getName(), n);
+            symbolMap.put(n.getIdent().getName(), new Symbol(n, currentScope));
             Type paramType = n.getType();
             if(paramType == Type.VOID) {
                 throw new TypeCheckException("Parameter " + symbolMap + " has no type");
@@ -76,9 +72,45 @@ public class SymbolTable {
                 throw new TypeCheckException("Parameter " + symbolMap + " is already defined");
             }
             else {
-                symbolMap.put(n.getIdent().getName(), n);
+                symbolMap.put(n.getIdent().getName(), new Symbol(n, currentScope));
             }
         }
     }
+
+}
+
+class Symbol {
+    //A symbol is literally like the exact same as a NameDef, but it's not an AST node
+    //Scope is also stored here, but I'm not sure how to implement it
+    private NameDef nameDef;
+
+    private int scope = 0;
+
+    private String name;
+
+    public Symbol(NameDef nameDef, int scope) {
+        this.nameDef = nameDef;
+        this.scope = scope;
+    }
+
+    public Symbol(Symbol symbol) {
+        this.nameDef = symbol.nameDef;
+        this.scope = symbol.scope;
+        this.name = symbol.name;
+    }
+
+    public NameDef getNameDef() {
+        return nameDef;
+    }
+
+    public int getScope() {
+        return scope;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
 
 }
